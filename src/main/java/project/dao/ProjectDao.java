a89afc4e89530d4a30789a1356ce7ce778bf54bd
Manuel Peralta <wowmanny26@gmail.com>
2023-04-06 21:07:17 -0700

    fourth commit

--- src/main/java/project/dao/ProjectDao.java
+++ src/main/java/project/dao/ProjectDao.java
@@ -18,7 +18,12 @@ import project.exception.DbException;
 import project.provided.util.DaoBase;
 
 public class ProjectDao extends DaoBase {
-
+	private static final String CATEGORY_TABLE = "category";
+	private static final String MATERIAL_TABLE = "material";
+	private static final String PROJECT_TABLE = "project";
+	private static final String PROJECT_CATEGORY_TABLE = "project_category";
+	private static final String STEP_TABLE = "step";
+	
 	public Project insertProject(Project project) {
 		//@formatter:off
 		String sql = ""
@@ -41,6 +46,7 @@ public class ProjectDao extends DaoBase {
 			stmt.executeUpdate();
 			
 			Integer projectId = getLastInsertId(conn, PROJECT_TABLE);
+			commitTransaction(conn);
 			
 			project.setProjectId(projectId);
 			return project;
@@ -57,13 +63,6 @@ public class ProjectDao extends DaoBase {
 	}
 		
 	
-
-	private static final String CATEGORY_TABLE = "category";
-	private static final String MATERIAL_TABLE = "material";
-	private static final String PROJECT_TABLE = "project";
-	private static final String PROJECT_CATEGORY_TABLE = "project_category";
-	private static final String STEP_TABLE = "step";
-	
 	
 	public List<Project> fetchAllProjects() {
 		String sql = "SELECT * FROM " + PROJECT_TABLE + " ORDER BY project_name";
@@ -74,13 +73,13 @@ public class ProjectDao extends DaoBase {
 			try(PreparedStatement stmt = conn.prepareStatement(sql)){
 	    	
 				try(ResultSet rs = stmt.executeQuery()){
-	    		List<Project> project = new LinkedList<>();
+	    		List<Project> projects = new LinkedList<>();
 	    		
 	    		while(rs.next()) {
-	    			project.add(extract(rs, Project.class));
+	    			projects.add(extract(rs, Project.class));
 	    			
 	    		}
-	    		return project;
+	    		return projects;
 	    	
 				}
 				
@@ -101,7 +100,7 @@ public class ProjectDao extends DaoBase {
 
 
 	public Optional<Project> fetchProjectById(Integer projectId) {
-	   String sql = "SELECT * FROM " + PROJECT_TABLE + "WHERE project_id = ?";
+	   String sql = "SELECT * FROM " + PROJECT_TABLE + " WHERE project_id = ?";
 	   
 	   try(Connection conn = DbConnection.getConnection()){
 		   startTransaction(conn);
@@ -120,7 +119,7 @@ public class ProjectDao extends DaoBase {
 			}
 		   }
 		   if(Objects.nonNull(project)) {
-			   project.getMaterials().addAll(fetchMaterialsForProject(conn,projectId));
+			   project.getMaterials().addAll(fetchMaterialsForProject(conn, projectId));
 			   project.getSteps().addAll(fetchStepsForProject(conn, projectId));
 			   project.getCategories().addAll(fetchCategoriesForProject(conn, projectId));
 		   }
@@ -168,7 +167,7 @@ public class ProjectDao extends DaoBase {
 
 
 	private List<Step> fetchStepsForProject(Connection conn, Integer projectId)throws SQLException {
-		String sql = "SELECT * FROM " + STEP_TABLE + "WHERE project_id = ?";
+		String sql = "SELECT * FROM " + STEP_TABLE + " WHERE project_id = ?";
 		
 		try(PreparedStatement stmt = conn.prepareStatement(sql)){
 			setParameter(stmt, 1, projectId, Integer.class);
@@ -186,7 +185,7 @@ public class ProjectDao extends DaoBase {
 
 
 	private List<Material> fetchMaterialsForProject(Connection conn, Integer projectId) throws SQLException {
-		String sql = "SELECT * FROM " + MATERIAL_TABLE + "WHERE project_id = ?";
+		String sql = "SELECT * FROM " + MATERIAL_TABLE + " WHERE project_id = ?";
 		  
 		try(PreparedStatement stmt = conn.prepareStatement(sql)){
 			  setParameter(stmt, 1, projectId, Integer.class);
@@ -201,6 +200,72 @@ public class ProjectDao extends DaoBase {
 			}
 		  }
 		
+	}
+
+
+
+	public boolean modifyProjectDetails(Project project) {
+		String sql = ""
+				+ "UPDATE " + PROJECT_TABLE + " SET "
+				+ "project_name = ?,"
+				+ "estimated_hours = ?,"
+				+ "actual_hours = ?,"
+				+ "difficulty = ?,"
+				+ "notes = ?"
+				+ "WHERE project_id = ?";
+		
+		try(Connection conn = DbConnection.getConnection()){
+		startTransaction(conn);
+		
+		try(PreparedStatement stmt = conn.prepareStatement(sql)){
+			setParameter(stmt, 1, project.getProjectName(), String.class);
+			setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+			setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+			setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+			setParameter(stmt, 5, project.getNotes(), String.class);
+			setParameter(stmt, 6, project.getProjectId(), Integer.class);
+			
+			boolean modified = stmt.executeUpdate() == 1;
+			commitTransaction(conn);
+			
+			return modified;
+			
+		}
+		catch(Exception e) {
+			rollbackTransaction(conn);
+			throw new DbException(e);
+		}
+			
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	}
+
+
+
+	public boolean deleteProject(Integer projectId) {
+		String sql = "DELETE FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+		
+		try(Connection conn = DbConnection.getConnection()){
+			startTransaction(conn);
+			
+		try(PreparedStatement stmt = conn.prepareStatement(sql)){
+			setParameter(stmt, 1, projectId, Integer.class);
+			
+			boolean deleted = stmt.executeUpdate() == 1;
+			
+			commitTransaction(conn);
+			return deleted;
+		}
+		catch(DbException e) {
+			rollbackTransaction(conn);
+			throw new DbException(e);
+		}
+			
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	
 	}
 	}
 
--- src/main/resources/project-schema.sql
+++ src/main/resources/project-schema.sql
@@ -28,6 +28,7 @@ FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
 UNIQUE KEY (project_id, category_id)
 );
 
+
 CREATE TABLE step(
 step_id INT AUTO_INCREMENT NOT NULL,
 project_id INT NOT NULL,
@@ -46,3 +47,24 @@ cost DECIMAL(7,2),
 PRIMARY KEY (material_id),
 FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
+
+-- Add some data
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Hang door', 3, 2, 3, 'need two people door is heavy');
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, 'Door hinges', 4, 3.50);
+INSERT INTO material (project_id, material_name, num_requred, cost) VALUES (1, 'SCREWS', 20, 4.50);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Align hangers on opening side of door frame', 1);
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Screw hangers into frame', 2);
+INSERT INTO category (category_id, category_name) VALUES (1, 'Doors and Windows');
+INSERT INTO category (category_id, category_name) VALUES (2, 'Repairs');
+INSERT INTO category (category_id, category_name) VALUES (3, 'Gardening');
+INSERT INTO project_category (project_id, category_id) VALUES (1, 1);
+INSERT INTO project_category (project_id, category_id) VALUES (1, 2);
+
+
+
+
+
+
+
+
